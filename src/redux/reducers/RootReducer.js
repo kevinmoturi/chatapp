85fd2f1ab26309966aae3a6aba1b7684f9edc9ea
Kevin Moturi <moturikev@gmail.com>
2022-04-08 01:07:18 +0300

    preserving chat messages for user joining in another tab

--- src/redux/reducers/RootReducer.js
+++ src/redux/reducers/RootReducer.js
@@ -1,10 +1,13 @@
 import  { combineReducers } from 'redux';
+import { withReduxStateSync } from 'redux-state-sync';
 
 // Listing Reducers
 import UserReducer from './UserReducer';
 import ChatReducer from './ChatReducer';
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user: UserReducer,
     chats: ChatReducer
 });
+
+export default withReduxStateSync(rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,20 @@ import  { createStore, applyMiddleware, compose } from 'redux';
 
 import thunk from 'redux-thunk';
 
+import { createStateSyncMiddleware, initStateWithPrevTab } from 'redux-state-sync'
+
 import rootReducer from './reducers/RootReducer';
 
 const initialState = {};
 
-const middleware = [thunk];
+const config = {
+    blacklist: ['SET_USER', 'GET_USER']
+}
+
+const middleware = [createStateSyncMiddleware(config), thunk];
 
 const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
+
+initStateWithPrevTab(store);
 
 export default store;
